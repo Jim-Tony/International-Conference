5f2fdd3e546c0e62e265b5f898791f71b410d4e4
Jim-Tony <jimtonystarc@gmail.com>
2022-03-26 23:50:24 +0530

    responsive done

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const abstractsLink=document.querySelector(".navbar-link");
+const homeLink=document.querySelector(".navbar-link");
 const mobileMenu = document.getElementById("menu");
 const content=document.querySelector(".container");
 const home = document.querySelector(".box-home");
@@ -7,6 +7,11 @@ const speakers = document.querySelector(".box-speakers");
 const committee = document.querySelector(".box-org-committee");
 const gallery = document.querySelector(".box-gallery");
 const navbar=document.querySelector(".navbar");
+const mobileNavbarLinks=document.querySelectorAll(".mobile");
+
+mobileNavbarLinks.forEach(m=>{
+    m.addEventListener("click",navToggle)
+})
 
 window.addEventListener("load",()=>{
     console.log("HERE")
@@ -15,6 +20,8 @@ window.addEventListener("load",()=>{
 })
 
 let currentPage=home;
+let currentActive=homeLink;
+currentActive.classList.add("active")
 
 navbar.addEventListener("click",(e)=>{
     let link=String(e.target.textContent)
@@ -26,6 +33,9 @@ navbar.addEventListener("click",(e)=>{
         home.classList.remove("box");
         home.classList.add("visible");
         currentPage=home;
+        currentActive && currentActive.classList.remove("active")
+        currentActive=e.target;
+        e.target.classList.add("active");
     }
     else if (link==='Submission of Abstract'){
         currentPage.classList.add("box");
@@ -33,6 +43,9 @@ navbar.addEventListener("click",(e)=>{
         abstract.classList.remove("box");
         abstract.classList.add("visible");
         currentPage=abstract;
+        currentActive && currentActive.classList.remove("active")
+        currentActive=e.target;
+        e.target.classList.add("active");
     }
     else if (link==='Invited Speakers'){
         currentPage.classList.add("box");
@@ -40,6 +53,9 @@ navbar.addEventListener("click",(e)=>{
         speakers.classList.remove("box");
         speakers.classList.add("visible");
         currentPage=speakers;
+        currentActive && currentActive.classList.remove("active")
+        currentActive=e.target;
+        e.target.classList.add("active");
     }
     else if (link==='Organizing Committee'){
         currentPage.classList.add("box");
@@ -47,6 +63,9 @@ navbar.addEventListener("click",(e)=>{
         committee.classList.remove("box");
         committee.classList.add("visible");
         currentPage=committee;
+        currentActive && currentActive.classList.remove("active")
+        currentActive=e.target;
+        e.target.classList.add("active");
     }
     else if (link==='Gallery'){
         currentPage.classList.add("box");
@@ -54,7 +73,11 @@ navbar.addEventListener("click",(e)=>{
         gallery.classList.remove("box");
         gallery.classList.add("visible");
         currentPage=gallery;
+        currentActive && currentActive.classList.remove("active")
+        currentActive=e.target;
+        e.target.classList.add("active");
     }
+    
 })
 // For Mobile menu
 mobileMenu.addEventListener("click",(e)=>{
